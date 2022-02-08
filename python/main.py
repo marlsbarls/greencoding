5fba689963d7fa75bdc6e089504b08087a089fc5
marlenebuhl <mbuhl@google.com>
2022-02-08 13:25:34 +0000

    Created Dockerfiles

--- python/main.py
+++ python/main.py
@@ -5,7 +5,8 @@ print(fibIter(fib_input))
 print(fibRec(fib_input))
 
 # merge sort
-from merge_sort import merge_sort
-from numpy import random
-array = random.randint(10000)
-merge_sort(array)
+#from merge_sort import merge_sort
+#from numpy import random
+#array = random.randint(10000)
+#array = [0,2,4,1,3,8]
+#print(merge_sort(array))
